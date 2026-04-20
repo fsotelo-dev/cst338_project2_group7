87403f6d02d782821c1cf226743765c3157ca082
Freddy Sotelo <fsotelo@csumb.edu>
2026-04-20 11:06:27 -0700

    Intialized Body

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * @author Freddy Sotelo
+ * created: 4/20/26
+ * @since Assignment: Default (Template) Project
+ **///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        }
+    }
+}
