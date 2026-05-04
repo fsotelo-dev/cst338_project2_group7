e451235725f4ae9e228bd4d7a7ce8dc6aa8a882e
am784 <angmagana@csumb.edu>
2026-05-04 12:46:42 -0700

    SignupController was made and runs as intended

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -1,7 +1,85 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
- * @author Freddy Sotelo
+ * @author App
  * created: 5/4/26
  * @since Assignment: App
  **/
 public class MainController {
+
+    public Scene buildScene() {
+        return null;
+    }
+//    public static Scene buildMAINScene(Stage stage) {
+//        Label title = new Label("Inferior");
+//        title.setStyle("-fx-text-fill: red; -fx-font-size: 50px; -fx-font-weight: bold;");
+//
+//
+//        HBox topBar = new HBox(title);
+//        topBar.setStyle("-fx-background-color: #1a1a1a; -fx-border-color: #2a2a2a; -fx-border-width: 0 0 1 0;");
+//
+//        // Left SIDEBAR
+//        Button homeBtn = new Button("⌂ Home");
+//        Button profileBtn = new Button("👤  Profile");
+//        Button settingsBtn = new Button("⚙   Settings");
+//        Button logoutBtn = new Button("⏻ Log Out");
+//
+//        for (Button btn : new Button[]{homeBtn, profileBtn, settingsBtn, logoutBtn}) {
+//            btn.setMaxWidth(Double.MAX_VALUE);
+//            btn.setStyle("-fx-text-fill: black; " +
+//                    "-fx-font-size: 14px; -fx-padding: 12 16;");
+//        }
+//
+//        homeBtn.setOnAction(e -> SceneManager.getInstance().navigateTo(SceneType.LOGIN));
+//        profileBtn.setOnAction(e -> SceneManager.getInstance().navigateTo(SceneType.PROFILE));
+//        settingsBtn.setOnAction(e -> SceneManager.getInstance().navigateTo(SceneType.SETTINGS));
+//        logoutBtn.setOnAction(e -> SceneManager.getInstance().navigateTo(SceneType.LOGIN));
+//
+//
+//        VBox sidebar = new VBox();
+//        sidebar.setPrefWidth(200);
+//        sidebar.setStyle("-fx-background-color: #1a1a1a; -fx-border-color: #2a2a2a; -fx-border-width: 0 1 0 0;");
+//
+//        // SideBar Buttons
+////        Button homeBtn = new Button("🏠  Home");
+////        Button profileBtn = new Button("👤  Profile");
+////        Button settingsBtn = new Button("⚙️   Settings");
+////        Button logoutBtn = new Button("↩  Log Out");
+//
+//        //Buttons Funtionality
+//        homeBtn.setOnAction(e ->
+//                SceneManager.getInstance().navigateTo(SceneType.SIGNUP));
+////                stage.setScene(create(SceneType.SIGNUP, stage)));
+//        homeBtn.setMaxWidth(Double.MAX_VALUE);
+////        profileBtn.setMaxWidth(Double.MAX_VALUE);
+////        profileBtn.setOnAction(e ->
+////                stage.setScene(create(SceneType.PROFILE, stage)));
+//
+//        settingsBtn.setMaxWidth(Double.MAX_VALUE);
+//
+//
+//        logoutBtn.setMaxWidth(Double.MAX_VALUE);
+//        logoutBtn.setOnAction(e ->
+//                stage.close());
+//
+//        StackPane mainContent = new StackPane();
+//        mainContent.setStyle("-fx-background-color: #141414;");
+//        BorderPane root = new BorderPane();
+//
+//        root.setTop(topBar);
+//        root.setLeft(sidebar);
+//        root.setCenter(mainContent);
+//
+//        VBox layout = new VBox(25, title, homeBtn, profileBtn, settingsBtn, logoutBtn);
+//        layout.setAlignment(Pos.CENTER);
+//        return new Scene(layout, 800, 600);
+//    }
 }
--- src/main/java/SignupController.java
+++ src/main/java/SignupController.java
@@ -1,7 +1,78 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
- * @author Freddy Sotelo
+ * @author Inferior
  * created: 5/4/26
  * @since Assignment: App
  **/
 public class SignupController {
+    private final DatabaseManager db = DatabaseManager.getInstance();
+
+    public Scene buildScene() {
+                Label title = new Label("Inferior");
+        title.setStyle("-fx-text-fill: red; -fx-font-size: 50px; -fx-font-weight: bold;");
+        title.setAlignment(Pos.TOP_CENTER);
+
+                Label signUp = new Label("Create account");
+        signUp.setStyle("-fx-font-size:20px; -fx-font-weight: bold;");
+        signUp.setAlignment(Pos.CENTER_RIGHT);
+
+                TextField usernameField = new TextField();
+        GridPane textLayout = new GridPane();
+        usernameField.setPromptText("Username");
+        TextField passwordField = new PasswordField();
+        passwordField.setPromptText("Password");
+
+                Button signupButton = new Button("     Sign Up     ");
+        signupButton.setStyle("-fx-border-color: red;");
+        Label status = new Label();
+        signupButton.setOnAction(e-> {
+            if(usernameField.getText().isEmpty()||passwordField.getText().isEmpty()){
+                status.setText("Please fill out all fields");
+                return;
+            }
+            db.insertItems(usernameField.getText(), passwordField.getText());
+
+            SceneManager.getInstance().navigateTo(SceneType.LOGIN);
+        });
+        textLayout.add(usernameField, 0, 0);
+        textLayout.add(passwordField, 0, 1);
+        textLayout.add(status, 0, 2);
+        textLayout.setAlignment(Pos.CENTER);
+
+                Button backTologinpage = new Button("Back to Login");
+        backTologinpage.setOnAction(e->
+                SceneManager.getInstance().navigateTo(SceneType.LOGIN));
+        backTologinpage.setAlignment(Pos.BOTTOM_RIGHT);
+
+        VBox layout = new VBox(25, title, signUp, textLayout, signupButton,backTologinpage);
+        layout.setAlignment(Pos.CENTER);
+        return new Scene(layout, 800, 600);
+
+    }
+//    public static Scene buildSIGNUPScene(Stage stage){
+//        DatabaseManager db = DatabaseManager.getInstance();
+//        //Only gets called when user clicks on Sign Up with a new page
+//
+//
+//
+//        Button backTologinpage = new Button("Back to Login");
+//        backTologinpage.setOnAction(e->
+//
+//                SceneManager.getInstance().navigateTo(SceneType.LOGIN));
+////                stage.setScene(create(SceneType.LOGIN,stage)));
+//        backTologinpage.setAlignment(Pos.BOTTOM_RIGHT);
+//
+//        VBox layout = new VBox(25, title, signUp, textLayout, signupButton,backTologinpage);
+//        layout.setAlignment(Pos.CENTER);
+//        return new Scene(layout, 800, 600);
+//    }
 }
