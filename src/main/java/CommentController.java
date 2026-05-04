e23200d65258db4749fd4114b4b46ae7df057800
F0rehe4d <devinhempel@outlook.com>
2026-05-04 12:55:49 -0700

    made Comment Scene and started the todo, waiting for post DAO

---
+++ src/main/java/CommentController.java
@@ -0,0 +1,8 @@
+public class CommentController {
+    //Todo: when PostDAO is ready create this controller
+    private int selectedPost;
+
+    public void setSelectedPost(int postID){
+        selectedPost = postID;
+    }
+}
