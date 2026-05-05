6cf00847f4d0752a7aa60007145e73b527db9148
f0rehe4d <devinhempel@outlook.com>
2026-05-05 15:57:22 -0700

    Redid post scene and post controller so that we can have it have multiple posts on the post page

--- src/main/java/PostController.java
+++ src/main/java/PostController.java
@@ -1,64 +1,80 @@
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
 
 public class PostController {
     public PostController(){
 
     }
     @FXML
-    private Label postUsernameLabel;
+    private VBox postFeedBox;
 
-    @FXML
-    private Label postHeadLabel;
-
-    @FXML
-    private Label postBodyLabel;
-
-    @FXML
-    private ListView<String> commentsListView;
-
-    @FXML
-    private Label postTierLabel;
-
-    @FXML
-    private Label likeCountLabel;
-            private int likeCount = 0;
     private int selectedPost = 1;
 
     @FXML
     public void initialize() {
-        comments();
+        showEmptyState();
     }
 
     @FXML
     public void back() {
         SceneManager.getInstance().navigateTo(SceneType.MAIN);
     }
-
+// ToDo: REPLACE THIS PROFILE BUTTON METHOD WITH CREATE POST BUTTON METHOD WHEN CREATED
     @FXML
     public void profile() {
         SceneManager.getInstance().navigateTo(SceneType.PROFILE);
     }
 
-    @FXML
-    public void like(){
-        likeCount++;
-        likeCountLabel.setText(likeCount + " Likes");
+    private void showEmptyState() {
+        postFeedBox.getChildren().clear();
+
+        Label empty = new Label("Zero Posts Yet!");
+        empty.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
+
+        postFeedBox.getChildren().add(empty);
     }
 
-    @FXML void createComment(){
-        SceneManager.getInstance().navigateTo(SceneType.COMMENT);
-    }
+    private VBox createPost(String username, String title, String body) {
+        Label postUsernameLabel = new Label(username);
+        Label commentsLabel = new Label("Comments:");
+        Label postTierLabel = new Label("Tier");
+        postTierLabel.setStyle("-fx-background-color: green; -fx-text-fill: white; -fx-padding: 5 10; -fx-background-radius: 10; -fx-font-weight: bold;");
 
-    @FXML
-    private void comments() {
-        commentsListView.getItems().clear();
+        Label postHeadLabel = new Label(title);
+        postHeadLabel.setStyle("-fx-font-weight: bold;");
+
+        Label postBodyLabel = new Label(body);
+
+        Button likeBtn = new Button("Like");
+        Label likeCountLabel = new Label("0 Likes");
+        Button commentBtn = new Button("Create Comment");
+
+        commentBtn.setOnAction(e ->
+                SceneManager.getInstance().navigateTo(SceneType.COMMENT)
+        );
+
+        ListView<String> commentsListView = new ListView<>();
+        commentsListView.setPrefHeight(120);
+
         CommentDAO commentDAO = new CommentDAO();
         commentsListView.getItems().addAll(commentDAO.getCommentsByPost(selectedPost));
+
+        HBox topRow = new HBox(10, postUsernameLabel, postTierLabel);
+        HBox actionRow = new HBox(10, likeBtn, likeCountLabel, commentBtn);
+
+        VBox card = new VBox(8, topRow, postHeadLabel, postBodyLabel, actionRow, commentsLabel, commentsListView);
+        card.setStyle("-fx-border-color: gray; -fx-padding: 10;");
+
+        return card;
     }
+
     public Scene buildScene(){
         try {
             FXMLLoader load = new FXMLLoader(SceneFactory.class.getResource("/PostScene.fxml"));
