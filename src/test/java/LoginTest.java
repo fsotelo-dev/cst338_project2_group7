c151b482419b14bb66dc0c4c36fd748e99f28872
Freddy Sotelo <fsotelo@csumb.edu>
2026-05-05 11:19:34 -0700

    Started Login Test

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,21 +3,43 @@
  * created: 5/4/26
  * @since Assignment: App
  **/
-import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
-import static org.junit.jupiter.api.Assertions.*;
+import javafx.stage.Stage;
 import static org.testfx.api.FxAssert.verifyThat;
-import static org.testfx.util.NodeQueryUtils.hasText;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
 
 public class LoginTest extends ApplicationTest{
     @Override
     public void start(Stage stage){
+        SceneManager.init(stage);
         SceneManager.getInstance().navigateTo(SceneType.LOGIN);
+        stage.show();
     }
 
     @Test
-    void loginScene_hasTitle(){
-        verifyThat("Inferior", hasText("Inferior"));
+    void testButtonClickededON(){
+        clickOn("loginBtn");
+        verifyThat("loginBtn", hasText("Login"));
+    }
+
+    @Test
+    void testLoginSuccess(){
+        clickOn("usernameField").write("admin");
+        clickOn("passwordField").write("12345");
+        clickOn("loginBtn");
+
+        verifyThat("loginBtn",hasText("Login successful"));
+
+    }
+    @Test
+    void testInvalidLoginShowsErrorMessage() {
+        clickOn("usernameField").write("wrongUser");
+        clickOn("passwordField").write("wrongPass");
+        clickOn("loginButton");
+
+        verifyThat("loginStatus", hasText("Incorrect username or password"));
     }
 }
